8edc1402efb6157046b4d787918bfd5d87febd7b
Denis0521 <87504630+Denis0521@users.noreply.github.com>
2026-06-21 19:30:05 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stock-portfolio-v4.1.1'; // ⚠️ 版本號已升至 3.9.5，與 HTML 同步
+const CACHE_NAME = 'stock-portfolio-v3.9.2'; // ⚠️ 版本號已升至 3.9.2，與 HTML 同步
 const urlsToCache = [
   './',
   './index.html',
@@ -11,7 +11,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache v4.1.1');
+        console.log('Opened cache v3.7.6');
         return cache.addAll(urlsToCache);
       })
   );
